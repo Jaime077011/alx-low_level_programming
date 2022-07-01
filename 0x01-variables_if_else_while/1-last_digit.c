45cecc1984b1cfd6fa3321fe9eba39e0433f9c32
JAIME077011 <jaime077011@example.com>
2022-07-01 16:01:24 -0700

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the last digit
+ * main - prints the last digit of the number
  * number stored in the variable n
- * return: always 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
