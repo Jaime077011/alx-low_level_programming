c07f6dd7a7d9ca4b3bcb72b4fa77fdc9d33feb42
JAIME077011 <jaime077011@example.com>
2022-07-05 05:41:14 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - prints the alphabet 10 times
+ */
+
+void print_alphabet_10x(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while(i < 10)
+	{
+		ch = 'a';
+		while(ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++
+	}
+}
