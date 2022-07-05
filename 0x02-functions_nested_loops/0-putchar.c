3a45c736d1878d92fd43fd250f289ee0c6380a52
JAIME077011 <jaime077011@example.com>
2022-07-05 01:51:39 -0700

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - it prints _putchar
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+	printf("_putchar\n");
+	return (0);
+}
