d1e47eaae03e38fcc1fe4fcf33d4438c633b8d36
JAIME077011 <jaime077011@example.com>
2022-07-05 08:09:50 -0700

    new

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,15 +4,14 @@
 /**
  * _abs - compute the absloute value of integer
  * @n: what we check
+ * return (0)
  */
 int _abs(int n)
 {
-	if(n >= 0)
+	if (n >= 0)
 	{
 		return (n);
-	} else
-	{
-		return (-n);
 	}
+	return (-n);
 }
 
