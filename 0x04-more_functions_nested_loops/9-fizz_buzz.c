e9eeebafae3e35d17c509edbc6d8bb1daf2bc2a7
JAIME077011 <jaime077011@example.com>
2022-07-07 08:17:53 -0700

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if ((i % 5 == 0) && (i % 3 == 0))
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz");
 		}
@@ -29,6 +29,7 @@ int main(void)
 		{
 			printf("%d", i);
 		}
+
 		if ( i != 100)
 		{
 			printf(" ");
