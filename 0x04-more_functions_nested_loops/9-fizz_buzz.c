5ee409963a5a4827cca0086ca037ca5c91471167
JAIME077011 <jaime077011@example.com>
2022-07-07 07:59:19 -0700

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,6 +4,7 @@
 /**
  * main - print fizz if the nuber divide by 3 ,
  * buzz if divieded by 5, fizzbuzz if both
+ * Return: always (0)
  */
 
 int main(void)
@@ -13,14 +14,24 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 15 == 0)
+		{
 			printf("FizzBuzz ");
+		}	
 		else if (i % 5 == 0)
+		{
 			printf("Buzz ");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else
-			printf(i );
+		{
+			printf("%d", i );
+		}
 	}
+	
 	printf("\n");
+	
 	return (0);
 }
