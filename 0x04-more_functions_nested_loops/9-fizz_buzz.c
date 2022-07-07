19d93d57c9f9018e30716198ddf68530c8e0a925
JAIME077011 <jaime077011@example.com>
2022-07-07 07:45:43 -0700

    new

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - print fizz if the nuber divide by 3 ,
+ * buzz if divieded by 5, fizzbuzz if both
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 15 == 1)
+			printf("FizzBuzz");
+		else if (i % 5 == 1)
+			printf("Buzz");
+		else if (i % 3 == 1)
+			printf("Fizz");
+		else
+			printf(i);
+	}
+}
