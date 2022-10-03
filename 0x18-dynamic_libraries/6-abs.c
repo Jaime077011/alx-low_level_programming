84b975ecff152d363da6b24b5487adf8de2c538f
JAIME077011 <jaime077011@gmail.com>
2022-10-03 20:36:54 +0000

    new

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - compute the absloute value of integer
+ * @n: what we check
+ * Return: (0)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
